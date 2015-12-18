db2310d57bb3be9dacc00736ecc1bbd6aed3c083
Sven <sven.community@gmail.com>
2015-12-18 15:03:08 +0100

    UI for sorting program v.1.3
    Updated to feature checkbox for pogopogosort. (I know it isn't written like that lmao)

--- Interface.java
+++ Interface.java
@@ -2,29 +2,30 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
-import java.util.Random;
-import java.util.Arrays;
 
-public class Interface extends JFrame {
-  
+
+
+public class SortingProgram extends JFrame {
+  // Anfang Attribute
   private JSlider slider = new JSlider();
-  private Button button = new Button();
+  private Button button1 = new Button();
   private Checkbox bubblechbx = new Checkbox();
   private Checkbox heapchbx = new Checkbox();
-  private Checkbox bogobogochbx = new Checkbox();
+  private Checkbox bogochbx = new Checkbox();
   private Checkbox cosmicraychbx = new Checkbox();
   private Checkbox quickchbx = new Checkbox();
   private Label label1 = new Label();
   private TextField showtime = new TextField();
   private Label label2 = new Label();
+  private Checkbox bogobogochbx = new Checkbox();
+  // Ende Attribute
   
-  
-  public Interface(String title) { 
-    
+  public SortingProgram(String title) { 
+    // Frame-Initialisierung
     super(title);
     setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
     int frameWidth = 493; 
-    int frameHeight = 387;
+    int frameHeight = 377;
     setSize(frameWidth, frameHeight);
     Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
     int x = (d.width - getSize().width) / 2;
@@ -33,76 +34,67 @@ public class Interface extends JFrame {
     setResizable(false);
     Container cp = getContentPane();
     cp.setLayout(null);
-    
+    // Anfang Komponenten
     
     slider.setBounds(16, 16, 358, 70);
-    slider.setMinorTickSpacing(10);
+    slider.setMinorTickSpacing(1000);
     slider.setMajorTickSpacing(2000);
     slider.setPaintTicks(true);
     slider.setPaintLabels(true);
     slider.setMaximum(10000);
     cp.add(slider);
-    button.setBounds(384, 24, 91, 305);
-    button.setLabel("START");
-    button.addActionListener(new ActionListener() { 
+    button1.setBounds(384, 24, 91, 305);
+    button1.setLabel("START");
+    button1.addActionListener(new ActionListener() { 
       public void actionPerformed(ActionEvent evt) { 
-        button_ActionPerformed(evt);
+        button1_ActionPerformed(evt);
       }
     });
-    button.setBackground(new Color(0xC0C0C0));
-    button.setFont(new Font("Impact", Font.PLAIN, 18));
-    cp.add(button);
+    button1.setBackground(new Color(0xC0C0C0));
+    button1.setFont(new Font("Impact", Font.PLAIN, 18));
+    cp.add(button1);
     bubblechbx.setBounds(24, 120, 100, 20);
     bubblechbx.setLabel("BubbleSort");
     cp.add(bubblechbx);
     heapchbx.setBounds(24, 152, 100, 20);
     heapchbx.setLabel("HeapSort");
     cp.add(heapchbx);
-    bogobogochbx.setBounds(24, 216, 116, 20);
-    bogobogochbx.setLabel("BogoBogoSort");
-    cp.add(bogobogochbx);
-    cosmicraychbx.setBounds(24, 248, 124, 20);
+    bogochbx.setBounds(24, 216, 116, 20);
+    bogochbx.setLabel("BogoSort");
+    cp.add(bogochbx);
+    cosmicraychbx.setBounds(24, 280, 124, 20);
     cosmicraychbx.setLabel("CosmicRaySort");
     cp.add(cosmicraychbx);
     quickchbx.setBounds(24, 184, 100, 20);
     quickchbx.setLabel("QuickSort");
     cp.add(quickchbx);
-    label1.setBounds(16, 80, 130, 36);
+    label1.setBounds(16, 80, 354, 28);
     label1.setText("LENGTH OF ARRAY");
     label1.setFont(new Font("Impact", Font.PLAIN, 18));
     cp.add(label1);
-    showtime.setBounds(152, 96, 222, 236);
+    showtime.setBounds(152, 120, 222, 212);
     cp.add(showtime);
-    label2.setBounds(16, 280, 110, 26);
+    label2.setBounds(16, 304, 110, 26);
     label2.setText("ALGORITHM");
     label2.setFont(new Font("Impact", Font.PLAIN, 18));
     cp.add(label2);
-    
+    bogobogochbx.setBounds(24, 248, 108, 20);
+    bogobogochbx.setLabel("BogoBogoSort");
+    cp.add(bogobogochbx);
+    // Ende Komponenten
     
     setVisible(true);
-  } 
+  } // end of public SortingProgram
   
-  public int randomInteger(int min, int max) {
-    Random rand = new Random();
-    int randomNum = rand.nextInt((max - min) + 1) + min;
+  // Anfang Methoden
+  public void button1_ActionPerformed(ActionEvent evt) {
     
-    return randomNum;
-  }
+  } // end of button1_ActionPerformed
   
-  private int[] randarr(int length) {
-    int[] a = new int[length];
-    for (int i = 0; i <= length - 1; i++) {
-      a[i] = 1 + randomInteger(1, 10000);
-    }
-    return a;
-  }
-  
-  public void button_ActionPerformed(ActionEvent evt) {
-    //INSERT CODE
-    int[] tbs = randarr(slider.getValue());
-  } 
+  // Ende Methoden
   
   public static void main(String[] args) {
-    new Interface("Interface");
-  } 
-} 
+    new SortingProgram("SortingProgram");
+  } // end of main
+  
+} // end of class SortingProgram
